1090670a070c1efc0eb414323afc4bc7360a405b
Tpalmer111 <mr.palmer111@gmail.com>
2022-10-21 17:37:07 -0600

    update nav bar

--- components/navBar.js
+++ components/navBar.js
@@ -1,7 +1,14 @@
 import Link from 'next/link'
+import Image from 'next/image'
 export default function NavBar() {
     return(
         <nav>
+            <Image
+                src='/denver_banner.jpeg'
+                alt='denver_banner'
+                width={1000}
+                height={300}
+            />
             <Link href='/'>Home</Link>
             <Link href='/about'>About Me</Link>
 
--- pages/about.js
+++ pages/about.js
@@ -3,20 +3,21 @@ import Image from 'next/image'
 export default function About() {
     return (
         <div>
-                <Image
-                src='/denver_banner.jpeg'
-                alt='denver_banner'
-                width={1000}
-                height={300}
-            />
+
             <h1>About me</h1>
             <p className={styles.extraGreen}>Fill in the about me content here. blah blah blah</p>
 
-            <Image
+            {/* <Image
                 src='https://placekitten.com/400/500'
                 alt='portfolio cat'
                 width={300}
                 height={300}
+            /> */}
+            <Image
+            src='/face.jpeg'
+            alt='head-shot'
+            width={300}
+            height={300}
             />
         </div>
     )
